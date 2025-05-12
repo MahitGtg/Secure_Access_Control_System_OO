d25c320af1f1acaa3420865867ae91e91b9f50cd
Aleksandra Lozyk <olalozyk@icloud.com>
2025-05-12 15:45:08 +0000

    Additng static to void function

--- src/stubs.c
+++ src/stubs.c
@@ -8,26 +8,24 @@
 
 // allow access to FILE-based IO (e.g. fprintf) in this translation unit
 #define CITS3007_PERMISSIVE
-
 #include "logging.h"
 #include "db.h"
-
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "banned.h"
 
 /**
- * Abort immediately for unrecoverable errors /
+ * Abort immediately for unrecoverable errors
  * invalid program state.
- * 
+ *
  * Arguments:
  * - msg: message to log before aborting
- * 
+ *
  * This function should not return.
  */
-void panic(const char *msg) {
+static void panic(const char *msg) {
   fprintf(stderr, "PANIC: %s\n", msg);
   abort();
 }
@@ -38,7 +36,6 @@ static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void log_message(log_level_t level, const char *fmt, ...) {
   pthread_mutex_lock(&log_mutex);
-
   va_list args;
   va_start(args, fmt);
   switch (level) {
@@ -59,32 +56,27 @@ void log_message(log_level_t level, const char *fmt, ...) {
       break;
   }
   vfprintf(stderr, fmt, args);
-  fprintf(stderr, "\n");  // newline, optional
+  fprintf(stderr, "\n"); // newline, optional
   va_end(args);
-
   pthread_mutex_unlock(&log_mutex);
 }
-
 
 bool account_lookup_by_userid(const char *userid, account_t *acc) {
   // This is a stub function. In a real implementation, this function would
   // query a database to find the account by user ID.
   // This implementation returns true and fills in a valid struct for userid "bob",
   // and returns false for all other user IDs.
-
   // Arguments must be non-null or behaviour is undefined; we choose to
   // abort in this case.
   if (!userid || !acc) {
     panic("Invalid arguments to account_lookup_by_userid");
   }
-
   // Example of a simple lookup. Note that no valid hashed password is set.
   // userid must be a valid, null-terminated string.
   // (Note that it is impossible in C for a function to check whether a string has been
   // properly null-terminated; this is always the responsibility of the caller.)
   if (strncmp(userid, "bob", USER_ID_LENGTH) == 0) {
     account_t bob_acc = { 0 };
-
     strcpy(bob_acc.userid, "bob");
     strcpy(bob_acc.email, "bob.smith@example.com");
     memcpy(bob_acc.birthdate, "1990-01-01", BIRTHDATE_LENGTH);
